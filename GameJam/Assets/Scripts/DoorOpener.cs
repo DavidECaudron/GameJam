86ce8c5d4d5375b872434735197e8721e755948e
inugasha <pirsoncorentin@live.be>
2022-03-07 12:19:43 +0100

    Update door

--- GameJam/Assets/Scripts/DoorOpener.cs
+++ GameJam/Assets/Scripts/DoorOpener.cs
@@ -1,12 +1,54 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class DoorOpener : MonoBehaviour
 {
-    public delegate void DoorOpenerActivated();
-    public event DoorOpenerActivated OpenDoorEvent;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private bool _uniqueUtilisation;
 
-    private void OpenDoor()
+    private bool _doorIsOpen;
+
+    public delegate void DoorOpenerActivated();
+    public event DoorOpenerActivated DoorOpenerEnableEvent;
+    public event DoorOpenerActivated DoorOpenerDisableEvent;
+
+    private void EnableOpener()
     {
-        OpenDoorEvent?.Invoke();
+        if (_doorIsOpen) return;
+
+        //_animator.SetTrigger("");
+
+        DoorOpenerEnableEvent?.Invoke();
+    }
+
+    private void DisableOpener()
+    {
+        if (_doorIsOpen) return;
+        DoorOpenerDisableEvent?.Invoke();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_doorIsOpen) return;
+        if (_uniqueUtilisation) return;
+        if (other.tag == "Player")
+        {
+            EnableOpener();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (_doorIsOpen) return;
+        if (_uniqueUtilisation) return;
+        if (other.tag == "Player")
+        {
+            DisableOpener();
+        }
+    }
+
+    public void DoorIsOpen()
+    {
+        _doorIsOpen = true;
     }
 }
