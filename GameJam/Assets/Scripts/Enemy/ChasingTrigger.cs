984bf81f1abeca919ea326e4dd621a6d41a25f98
inugasha <53877578+inugasha@users.noreply.github.com>
2022-03-09 13:44:35 +0100

    Player die

--- GameJam/Assets/Scripts/Enemy/ChasingTrigger.cs
+++ GameJam/Assets/Scripts/Enemy/ChasingTrigger.cs
@@ -4,9 +4,14 @@ public class ChasingTrigger : MonoBehaviour
 {
     [SerializeField] private FollowTarget _followTarget;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.tag != "Player") return;
+        if (_followTarget.OnChasing()) return;
+
+        ModelController model = other.GetComponent<ModelController>();
+        if (!model.Alive) return;
+
         _followTarget.StartChasing(other.transform);
     }
 }
